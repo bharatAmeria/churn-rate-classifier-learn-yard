adb050577d35928cd4c9b08c5a8016068c005769
bharatAmeria <21001019007@jcboseust.ac.in>
2025-06-28 08:37:30 +0530

    app and actions flow setup

--- src/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-from pathlib import Path
-
-def from_root(relative_path=""):
-    """Get the absolute path from the project root."""
-    return os.path.join(Path(__file__).resolve().parents[2], relative_path)
